fbd5e9c13808c96e086b5aa718616df44315766c
7132 <17630307132@163.com>
2020-05-21 01:30:55 +0800

    提交测试

---
+++ main/java/com/example/demoo/controller/HelloWordController.java
@@ -0,0 +1,37 @@
+package com.example.demoo.controller;
+
+
+import com.example.demoo.entity.User;
+import com.example.demoo.service.HelloWord;
+import com.example.demoo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class HelloWordController {
+
+    @Autowired
+    HelloWord helloWord;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/say")
+    String sayHelloWord() {
+        return helloWord.sayHelloWord();
+    }
+
+    @RequestMapping("/query.htm")
+    ModelAndView queryUserByName() {
+        ModelAndView vm=new ModelAndView();
+        List<User> users = userService.queryUserByName();
+        vm.addObject("users",users);
+        vm.setViewName("login");
+        return vm;
+    }
+}
